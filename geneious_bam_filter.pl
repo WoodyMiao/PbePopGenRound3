fbc156e9243635cabfb8ab7edf969ed84c079e6f
WoodyMiao <miao_lin@163.com>
2016-05-06 15:59:50 +0800

    add geneious_bam_filter

---
+++ geneious_bam_filter.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+open I, "-|", "samtools-0.1.7 view -h $ARGV[0]";
+open O, "|-", "samtools-0.1.7 view -Shb - >$ARGV[1]";
+
+my $total = 0;
+my $remain = 0;
+while (<I>) {
+	if (/^\@/) {
+		if (/^\@HD/) {
+			print O "\@HD\tVN:1.3\tSO:coordinate\n";
+		} elsif (/^\@SQ/) {
+			print O $_;
+		} elsif (/^\@RG/) {
+			next;
+		} 
+	} else {
+		++$total;
+		my @a = split /\t/;
+		if (($a[1] == 83 or $a[1] == 99 or $a[1] == 147 or $a[1] == 163) and ($a[6] eq "=")) {
+				++$remain;
+				my @b = split / /, $a[0];
+				shift @a;
+				pop @a;
+				print O "$b[0]\t", join("\t", @a), "\n";
+		} else {
+			next;
+		}
+	}
+}
+warn "$out\t$total\t$remain\n";
+close I;
+close O;
